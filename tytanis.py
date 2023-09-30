4dbfb6cfd91476b9d431bff725980615b18ae167
Oskar Gunther <oskar.gunther94@gmail.com>
2023-09-30 21:35:43 +0200

    Lepsze kolorki

--- tytanis.py
+++ tytanis.py
@@ -45,13 +45,14 @@ def quantum_shit(x, y, z):
     return counts['00'] / counts['11']
 
 def quantum_shit2(position, scale):
+    print(position, scale)
     simulator = AerSimulator()
     qreg_q = QuantumRegister(1, 'q')
     creg_c = ClassicalRegister(1, 'c')
     color_circuit = QuantumCircuit(qreg_q, creg_c)
     color_circuit.reset(qreg_q[0])
     color_circuit.x(qreg_q[0])
-    color_circuit.rx(pi/(2 + (position / scale)), qreg_q[0])
+    color_circuit.rx(pi/(2 + ((position / scale)*4)), qreg_q[0])
     color_circuit.measure(qreg_q[0], creg_c[0])
     compiled_circuit = transpile(color_circuit, simulator)
     job = simulator.run(compiled_circuit, shots=100)
@@ -61,10 +62,10 @@ def quantum_shit2(position, scale):
     print(counts)
  
     if '0' not in counts:
-        counts['0'] = 0.001
+        counts['0'] = 1
  
     if '1' not in counts:
-        counts['1'] = 0.001
+        counts['1'] = 1
  
     result = counts['0'] / counts['1']
  
@@ -80,19 +81,17 @@ quantum_shit(1, 2, 3)
 
 def lorenz(xyz, *, s=10, r=28, b=2.667):
     x, y, z = xyz
-    colors = []
 
-    asd = sqrt(quantum_shit(x, y, z))
-    x_dot = s * (y - x) * asd
-    y_dot = r * x - y - x * z * asd
-    z_dot = x * y - b * z * asd
-    return np.array([x_dot, y_dot, z_dot]), [quantum_shit2(x_dot, 100), quantum_shit2(y_dot, 40), quantum_shit2(z_dot, 40)]
+    x_dot = s * (y - x)
+    y_dot = r * x - y - x * z
+    z_dot = x * y - b * z
+    return np.array([x_dot, y_dot, z_dot]), [quantum_shit2(x_dot, 200), quantum_shit2(abs(y_dot), 100), quantum_shit2(abs(z_dot), 100)]
 
 
 dt = 0.01
 
 xyzs = np.empty((steps + 1, 3))  # Need one more for the initial values
-xyzs[0] = (0., 1., 1.05)  # Set initial values
+xyzs[0] = (0., 1. + quantum_shit(1, 0,0), 1.05 + quantum_shit(1, 0, 0))  # Set initial values
 # Step through "time", calculating the partial derivatives at the current point
 # and using them to estimate the next point
 colors = []
@@ -136,7 +135,7 @@ ax.grid(False)
 ax.set_axis_off()
 
 
-ax.scatter(*xyzs.T, lw=0.5, s=0.4, c=np.array(colors))
+ax.scatter(*xyzs.T, lw=0.5, s=1, c=np.array(colors))
 ax.set_xlabel("X Axis")
 ax.set_ylabel("Y Axis")
 ax.set_zlabel("Z Axis")
